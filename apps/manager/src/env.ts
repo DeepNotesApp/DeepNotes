4b447f1f77acbfe3bbb2bb48aa4402a0d570a46d
Gustavo Toyota <gustavottoyota@gmail.com>
2023-03-18 20:08:19 -0400

    feat: implement manager service

---
+++ apps/manager/src/env.ts
@@ -0,0 +1,15 @@
+import { once } from '@stdlib/misc';
+import dotenv from 'dotenv';
+import * as dotenvExpand from 'dotenv-expand';
+
+Object.defineProperty(process, 'env', {
+  get: once(() => {
+    dotenvExpand.expand(
+      dotenv.config({ path: '../../.env' }).parsed?.DEV
+        ? dotenv.config({ path: '../../.env.dev' })
+        : dotenv.config({ path: '../../.env.prod' }),
+    );
+
+    return process.env;
+  }, process.env),
+});
