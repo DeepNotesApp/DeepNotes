e8e2d53e46f27ff0eb02b74e56bcd25b0b56555b
Gustavo Toyota <gustavottoyota@gmail.com>
2023-04-03 09:16:56 -0400

    improve(client): expand left sidebar by default on wide screens

--- apps/client/src/boot/ui.client.ts
+++ apps/client/src/boot/ui.client.ts
@@ -1,6 +1,8 @@
 import { boot } from 'quasar/wrappers';
 
 export default boot(({ store }) => {
+  uiStore(store).leftSidebarExpanded = window.innerWidth > 1000;
+
   uiStore(store).currentPathExpanded =
     internals.localStorage.getItem('currentPathExpanded') !== 'false';
   uiStore(store).recentPagesExpanded =
