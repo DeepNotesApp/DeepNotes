605be58f045eb35b44cb2cb61c97bcbd959a6920
Gustavo Toyota <gustavottoyota@gmail.com>
2023-04-16 08:16:48 -0400

    improve: add pages.deletePermanently procedure

--- apps/app-server-trpc/src/api/pages/deletion/index.ts
+++ apps/app-server-trpc/src/api/pages/deletion/index.ts
@@ -2,9 +2,11 @@ import { once } from 'lodash';
 import { trpc } from 'src/trpc/server';
 
 import { deleteProcedure } from './delete';
+import { deletePermanentlyProcedure } from './delete-permanently';
 
 export const deletionRouter = once(() =>
   trpc.router({
     delete: deleteProcedure(),
+    deletePermanently: deletePermanentlyProcedure(),
   }),
 );
