8fed294a1d4b4953e445208a1b0a4b9f1cff8275
Gustavo Toyota <gustavottoyota@gmail.com>
2023-04-24 10:23:22 -0400

    fix(scheduler): fix scheduler

--- apps/scheduler/src/index.ts
+++ apps/scheduler/src/index.ts
@@ -6,10 +6,7 @@ import {
   GroupMemberModel,
   GroupModel,
   PageModel,
-  SessionModel,
-  UserModel,
 } from '@deeplib/db';
-import { addDays } from '@stdlib/misc';
 
 import { dataAbstraction } from './data/data-abstraction';
 import { initKnex } from './data/knex';
@@ -25,10 +22,6 @@ setInterval(async () => {
     groupMembers,
 
     permanentlyDeletedGroups,
-
-    invalidatedSessions,
-
-    expiredDemoUsers,
   ] = await Promise.all([
     PageModel.query()
       .join('groups', 'groups.id', 'pages.group_id')
@@ -48,12 +41,6 @@ setInterval(async () => {
 
     GroupModel.query()
       .where('permanent_deletion_date', '>', new Date())
-      .select('id'),
-
-    SessionModel.query().where('invalidated', true).select('id'),
-
-    UserModel.query()
-      .where('creation_date', '<', addDays(new Date(), -7))
       .select('id'),
   ]);
 
@@ -90,14 +77,6 @@ setInterval(async () => {
 
       ...permanentlyDeletedGroups.map((group) =>
         dataAbstraction().delete('group', group.id, { dtrx }),
-      ),
-
-      ...invalidatedSessions.map((session) =>
-        dataAbstraction().delete('session', session.id, { dtrx }),
-      ),
-
-      ...expiredDemoUsers.map((user) =>
-        dataAbstraction().delete('user', user.id, { dtrx }),
       ),
     ]);
   });
