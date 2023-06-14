f287e79281056f1e9b7f3e41f36f5d0363486df0
Gustavo Toyota <gustavottoyota@gmail.com>
2023-06-14 15:42:18 -0400

    improve(electron): add auto updater

--- apps/client/src-electron/electron-main.ts
+++ apps/client/src-electron/electron-main.ts
@@ -2,6 +2,40 @@ import { app, BrowserWindow, ipcMain, nativeTheme } from 'electron';
 import cookie from 'cookie';
 import path from 'path';
 import os from 'os';
+import log from 'electron-log';
+import { autoUpdater } from 'electron-updater';
+
+log.transports.file.level = 'info';
+
+log.info('App starting...');
+
+autoUpdater.on('checking-for-update', () => {
+  sendStatusToWindow('Checking for update...');
+});
+autoUpdater.on('update-available', (info) => {
+  sendStatusToWindow('Update available.');
+});
+autoUpdater.on('update-not-available', (info) => {
+  sendStatusToWindow('Update not available.');
+});
+autoUpdater.on('error', (err) => {
+  sendStatusToWindow('Error in auto-updater. ' + err);
+});
+autoUpdater.on('download-progress', (progressObj) => {
+  let log_message = 'Download speed: ' + progressObj.bytesPerSecond;
+  log_message = log_message + ' - Downloaded ' + progressObj.percent + '%';
+  log_message =
+    log_message +
+    ' (' +
+    progressObj.transferred +
+    '/' +
+    progressObj.total +
+    ')';
+  sendStatusToWindow(log_message);
+});
+autoUpdater.on('update-downloaded', (info) => {
+  sendStatusToWindow('Update downloaded');
+});
 
 // needed in case process is undefined under Linux
 const platform = process.platform || os.platform();
@@ -15,6 +49,11 @@ try {
 } catch (_) {}
 
 let mainWindow: BrowserWindow | undefined;
+
+function sendStatusToWindow(text: string) {
+  log.info(text);
+  mainWindow?.webContents.send('message', text);
+}
 
 function createWindow() {
   /**
@@ -149,7 +188,15 @@ function createWindow() {
   );
 }
 
-app.whenReady().then(createWindow);
+app.whenReady().then(() => {
+  createWindow();
+
+  try {
+    autoUpdater.checkForUpdatesAndNotify();
+  } catch (err) {
+    log.error(err);
+  }
+});
 
 app.on('window-all-closed', () => {
   if (platform !== 'darwin') {
