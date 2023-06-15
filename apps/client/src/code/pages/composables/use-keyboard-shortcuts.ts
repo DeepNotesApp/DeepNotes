0517eeba741eb1ea423a8691179fc1c8c774edd3
Gustavo Toyota <gustavottoyota@gmail.com>
2023-06-15 16:35:57 -0400

    fix(pages): fix conflicting shortcuts

--- apps/client/src/code/pages/composables/use-keyboard-shortcuts.ts
+++ apps/client/src/code/pages/composables/use-keyboard-shortcuts.ts
@@ -6,6 +6,7 @@ export function useKeyboardShortcuts() {
   useEventListener('keydown', async (event) => {
     if (await onKeyDown(event)) {
       event.preventDefault();
+      event.stopPropagation();
     }
   });
 
@@ -16,6 +17,18 @@ export function useKeyboardShortcuts() {
 
     if (page == null) {
       return;
+    }
+
+    const target = event.target as HTMLElement;
+
+    const isEditing =
+      target.nodeName === 'INPUT' ||
+      target.nodeName === 'TEXTAREA' ||
+      target.isContentEditable;
+
+    if (isEditing && event.code === 'Escape') {
+      page.editing.stop();
+      return true;
     }
 
     if (event.code === 'Escape' && page.findAndReplace.react.active) {
@@ -44,133 +57,118 @@ export function useKeyboardShortcuts() {
       return true;
     }
 
-    if (isCtrlDown(event) && event.code === 'KeyH') {
+    if (isCtrlDown(event) && event.code === 'KeyH' && !isEditing) {
       page.findAndReplace.react.active =
         !page.findAndReplace.react.active || !page.findAndReplace.react.replace;
       page.findAndReplace.react.replace = true;
       return true;
     }
 
-    // If currently editing something
+    if (!isEditing) {
+      const activeElem = page.activeElem.react.value;
 
-    const target = event.target as HTMLElement;
+      // Basic
 
-    if (
-      target.nodeName === 'INPUT' ||
-      target.nodeName === 'TEXTAREA' ||
-      target.isContentEditable
-    ) {
-      if (event.code === 'Escape') {
-        page.editing.stop();
+      if (isCtrlDown(event) && event.code === 'KeyX') {
+        page.clipboard.cut();
+        return true;
+      }
+      if (isCtrlDown(event) && event.code === 'KeyC') {
+        page.clipboard.copy();
+        return true;
+      }
+      if (isCtrlDown(event) && event.code === 'KeyV' && window.clipboardData) {
+        await page.clipboard.paste();
+        return true;
+      }
+      if (isCtrlDown(event) && event.code === 'KeyD') {
+        await page.cloning.perform();
         return true;
       }
 
-      return;
-    }
+      if (isCtrlDown(event) && event.code === 'KeyZ') {
+        page.undoRedo.undo();
+        return true;
+      }
+      if (isCtrlDown(event) && event.code === 'KeyY') {
+        page.undoRedo.redo();
+        return true;
+      }
 
-    const activeElem = page.activeElem.react.value;
+      if (isCtrlDown(event) && event.code === 'KeyA') {
+        page.selection.selectAll();
+        return true;
+      }
+      if (event.code === 'Delete') {
+        page.deleting.perform();
+        return true;
+      }
 
-    // Basic
+      // Formatting
 
-    if (isCtrlDown(event) && event.code === 'KeyX') {
-      page.clipboard.cut();
-      return true;
-    }
-    if (isCtrlDown(event) && event.code === 'KeyC') {
-      page.clipboard.copy();
-      return true;
-    }
-    if (isCtrlDown(event) && event.code === 'KeyV' && window.clipboardData) {
-      await page.clipboard.paste();
-      return true;
-    }
-    if (isCtrlDown(event) && event.code === 'KeyD') {
-      await page.cloning.perform();
-      return true;
-    }
+      if (isCtrlDown(event) && event.code === 'KeyB') {
+        page.selection.toggleMark('bold');
+        return true;
+      }
+      if (isCtrlDown(event) && event.code === 'KeyI') {
+        page.selection.toggleMark('italic');
+        return true;
+      }
+      if (isCtrlDown(event) && event.code === 'KeyU') {
+        page.selection.toggleMark('underline');
+        return true;
+      }
+      if (isCtrlDown(event) && event.code === 'KeyE') {
+        page.selection.toggleMark('code');
+        return true;
+      }
+      if (isCtrlDown(event) && event.shiftKey && event.code === 'KeyX') {
+        page.selection.toggleMark('strike');
+        return true;
+      }
 
-    if (isCtrlDown(event) && event.code === 'KeyZ') {
-      page.undoRedo.undo();
-      return true;
-    }
-    if (isCtrlDown(event) && event.code === 'KeyY') {
-      page.undoRedo.redo();
-      return true;
-    }
+      if (isCtrlDown(event) && event.altKey && event.code === 'Digit1') {
+        page.selection.toggleNode('heading', { level: 1 });
+        return true;
+      }
+      if (isCtrlDown(event) && event.altKey && event.code === 'Digit2') {
+        page.selection.toggleNode('heading', { level: 2 });
+        return true;
+      }
+      if (isCtrlDown(event) && event.altKey && event.code === 'Digit3') {
+        page.selection.toggleNode('heading', { level: 3 });
+        return true;
+      }
 
-    if (isCtrlDown(event) && event.code === 'KeyA') {
-      page.selection.selectAll();
-      return true;
-    }
-    if (event.code === 'Delete') {
-      page.deleting.perform();
-      return true;
-    }
+      // Others
 
-    // Formatting
+      if (event.code === 'F2' && activeElem != null) {
+        await page.editing.start(activeElem);
+        return true;
+      }
 
-    if (isCtrlDown(event) && event.code === 'KeyB') {
-      page.selection.toggleMark('bold');
-      return true;
-    }
-    if (isCtrlDown(event) && event.code === 'KeyI') {
-      page.selection.toggleMark('italic');
-      return true;
-    }
-    if (isCtrlDown(event) && event.code === 'KeyU') {
-      page.selection.toggleMark('underline');
-      return true;
-    }
-    if (isCtrlDown(event) && event.code === 'KeyE') {
-      page.selection.toggleMark('code');
-      return true;
-    }
-    if (isCtrlDown(event) && event.shiftKey && event.code === 'KeyX') {
-      page.selection.toggleMark('strike');
-      return true;
-    }
+      if (event.code === 'Backspace' && activeElem != null) {
+        await page.editing.start(activeElem);
+        page.editing.react.editor?.commands.deleteSelection();
+        return true;
+      }
 
-    if (isCtrlDown(event) && event.altKey && event.code === 'Digit1') {
-      page.selection.toggleNode('heading', { level: 1 });
-      return true;
-    }
-    if (isCtrlDown(event) && event.altKey && event.code === 'Digit2') {
-      page.selection.toggleNode('heading', { level: 2 });
-      return true;
-    }
-    if (isCtrlDown(event) && event.altKey && event.code === 'Digit3') {
-      page.selection.toggleNode('heading', { level: 3 });
-      return true;
-    }
-
-    // Others
-
-    if (event.code === 'F2' && activeElem != null) {
-      await page.editing.start(activeElem);
-      return true;
-    }
-
-    if (event.code === 'Backspace' && activeElem != null) {
-      await page.editing.start(activeElem);
-      page.editing.react.editor?.commands.deleteSelection();
-      return true;
-    }
-
-    if (event.code === 'ArrowLeft') {
-      page.selection.shift(new Vec2(-1, 0));
-      return true;
-    }
-    if (event.code === 'ArrowRight') {
-      page.selection.shift(new Vec2(1, 0));
-      return true;
-    }
-    if (event.code === 'ArrowUp') {
-      page.selection.shift(new Vec2(0, -1));
-      return true;
-    }
-    if (event.code === 'ArrowDown') {
-      page.selection.shift(new Vec2(0, 1));
-      return true;
+      if (event.code === 'ArrowLeft') {
+        page.selection.shift(new Vec2(-1, 0));
+        return true;
+      }
+      if (event.code === 'ArrowRight') {
+        page.selection.shift(new Vec2(1, 0));
+        return true;
+      }
+      if (event.code === 'ArrowUp') {
+        page.selection.shift(new Vec2(0, -1));
+        return true;
+      }
+      if (event.code === 'ArrowDown') {
+        page.selection.shift(new Vec2(0, 1));
+        return true;
+      }
     }
   }
 }
