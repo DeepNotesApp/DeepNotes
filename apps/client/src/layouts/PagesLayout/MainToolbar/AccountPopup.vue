6b92b19143ddf089dac65fe610c088f5b6af7203
Gustavo Toyota <gustavottoyota@gmail.com>
2023-07-09 09:14:06 -0400

    visual(pages): add links to account popup

--- apps/client/src/layouts/PagesLayout/MainToolbar/AccountPopup.vue
+++ apps/client/src/layouts/PagesLayout/MainToolbar/AccountPopup.vue
@@ -31,16 +31,6 @@
             </q-item-section>
             <q-item-section>Account settings</q-item-section>
           </q-item>
-
-          <q-item
-            clickable
-            @click="logout()"
-          >
-            <q-item-section avatar>
-              <q-icon name="mdi-logout" />
-            </q-item-section>
-            <q-item-section>Logout</q-item-section>
-          </q-item>
         </template>
 
         <template v-else>
@@ -64,13 +54,59 @@
             <q-item-section>Register</q-item-section>
           </q-item>
         </template>
+
+        <q-item
+          clickable
+          :href="multiModePath('/pricing')"
+        >
+          <q-item-section avatar>
+            <q-icon name="mdi-currency-usd" />
+          </q-item-section>
+          <q-item-section>Pricing</q-item-section>
+        </q-item>
+
+        <q-item
+          v-if="isIncluded(quasarMode, ['ssr', 'spa'])"
+          clickable
+          :href="multiModePath('/download')"
+        >
+          <q-item-section avatar>
+            <q-icon name="mdi-download" />
+          </q-item-section>
+          <q-item-section>Download</q-item-section>
+        </q-item>
+
+        <q-item
+          clickable
+          :href="multiModePath('/whitepaper')"
+        >
+          <q-item-section avatar>
+            <q-icon name="mdi-file-document" />
+          </q-item-section>
+          <q-item-section>Whitepaper</q-item-section>
+        </q-item>
       </q-list>
+
+      <template v-if="uiStore().loggedIn">
+        <q-item
+          clickable
+          @click="logout()"
+        >
+          <q-item-section avatar>
+            <q-icon name="mdi-logout" />
+          </q-item-section>
+          <q-item-section>Logout</q-item-section>
+        </q-item>
+      </template>
     </q-menu>
   </ToolbarBtn>
 </template>
 
 <script setup lang="ts">
+import { isIncluded } from '@stdlib/misc';
 import { logout } from 'src/code/auth/logout';
 import { selfUserName } from 'src/code/self-user-name';
 import { multiModePath } from 'src/code/utils/misc';
+
+const quasarMode = process.env.MODE;
 </script>
