c6bdce919b89b2cd8a0ef6abb9c91c85bf174de5
Gustavo Toyota <gustavottoyota@gmail.com>
2023-07-24 14:49:02 -0400

    visual(home): bump download version

--- apps/client/src/pages/home/Download/Download.vue
+++ apps/client/src/pages/home/Download/Download.vue
@@ -212,7 +212,7 @@ import { multiModePath } from 'src/code/utils/misc';
 
 import PlatformCard from './PlatformCard.vue';
 
-const version = '1.0.3';
+const version = '1.0.4';
 
 const loading = ref(true);
 
