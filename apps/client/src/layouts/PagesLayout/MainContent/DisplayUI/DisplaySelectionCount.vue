cbdafbe98003d480583c75d87d78623376e932d8
Gustavo Toyota <gustavottoyota@gmail.com>
2023-09-15 14:13:58 -0400

    feat(pages): add selection count UI

---
+++ apps/client/src/layouts/PagesLayout/MainContent/DisplayUI/DisplaySelectionCount.vue
@@ -0,0 +1,27 @@
+<template>
+  <div
+    v-if="page.selection.react.elems.length > 0"
+    class="selection-count"
+  >
+    {{ page.selection.react.elems.length }}
+    item{{ page.selection.react.elems.length > 1 ? 's' : '' }} selected
+  </div>
+</template>
+
+<script setup lang="ts">
+const page = computed(() => internals.pages.react.page);
+</script>
+
+<style scoped lang="scss">
+.selection-count {
+  position: absolute;
+
+  bottom: 12px;
+  right: 14px;
+
+  font-size: 12px;
+  color: lighten(#006dd2, 23%);
+
+  pointer-events: none;
+}
+</style>
