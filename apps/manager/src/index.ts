1e10032722ea652b3d2b492baad66d625df8b5eb
Gustavo Toyota <gustavottoyota@gmail.com>
2023-11-04 10:35:44 -0400

    feat: implement basic manager

--- apps/manager/src/index.ts
+++ apps/manager/src/index.ts
@@ -1,7 +1,50 @@
 import './env';
 
+import readline from 'readline';
+
+import { dataAbstraction } from './data/data-abstraction';
 import { initKnex } from './data/knex';
 
 initKnex();
 
-// Write code here
+const readlineInterface = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+async function handleCommand(command: string) {
+  const [commandName, ...args] = command.split(' ');
+
+  switch (commandName) {
+    case 'help':
+      console.log('Commands:');
+      console.log('- hget <prefix> <suffix> <field>');
+      console.log('- hset <prefix> <suffix> <field> <value>');
+      break;
+    case 'hget':
+      console.log(
+        await dataAbstraction().hget(args[0] as any, args[1], args[2]),
+      );
+      break;
+    case 'hset':
+      await dataAbstraction().hmset(args[0] as any, args[1], {
+        [args[2]]: args[3],
+      });
+      break;
+  }
+}
+
+function requestCommand() {
+  readlineInterface.question('', async (command) => {
+    if (command === 'exit') {
+      readlineInterface.close();
+      return;
+    }
+
+    await handleCommand(command);
+
+    requestCommand();
+  });
+}
+
+requestCommand();
