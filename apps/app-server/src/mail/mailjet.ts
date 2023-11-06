ebf51109b10998293a5665cbc90a352f9810eee7
Gustavo Toyota <gustavottoyota@gmail.com>
2023-11-06 14:11:27 -0400

    chore: update deps breaking

--- apps/app-server/src/mail/mailjet.ts
+++ apps/app-server/src/mail/mailjet.ts
@@ -19,7 +19,7 @@ export async function sendMailjetMail({
   subject,
   html,
 }: MailOptions) {
-  const data: SendEmailV3_1.IBody = {
+  const data: SendEmailV3_1.Body = {
     Messages: [
       {
         From: {
