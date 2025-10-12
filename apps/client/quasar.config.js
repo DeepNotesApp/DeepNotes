0006f19389a43fc3820f5b037d8aeb84c485d961
Gustavo Toyota <gustavottoyota@gmail.com>
2025-10-12 20:55:05 -0300

    fix: bigint problem

--- apps/client/quasar.config.js
+++ apps/client/quasar.config.js
@@ -98,7 +98,7 @@ module.exports = configure(function (ctx) {
     // Full list of options: https://v2.quasar.dev/quasar-cli-vite/quasar-config-js#build
     build: {
       target: {
-        browser: ['es2019', 'edge88', 'firefox78', 'chrome87', 'safari13.1'],
+        browser: ['es2020', 'edge88', 'firefox78', 'chrome87', 'safari14'],
         node: 'node16',
       },
 
