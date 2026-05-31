b07da808f639b34fb323e8d3502ee36103dc7e91
Gustavo Toyota <gustavottoyota@gmail.com>
2026-05-31 01:29:06 -0300

    fix: lint error

--- new-deepnotes/apps/api-worker/src/index.ts
+++ new-deepnotes/apps/api-worker/src/index.ts
@@ -28,7 +28,6 @@ export default {
   async scheduled(
     _event: ScheduledEvent,
     env: Bindings,
-    _ctx: ExecutionContext,
   ) {
     const hyper = env.HYPERDRIVE;
     if (hyper == null) {
